fbbf006ab86c82a6f4b02e9283f6e027bb466f2d
Joel Bremson <joel.bremson@ibm.com>
2025-02-20 19:43:44 -0800

    Added dns support to the vpc.

--- setup/main.tf
+++ setup/main.tf
@@ -4,7 +4,9 @@ provider "aws" {
 
 # VPC Configuration
 resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
   tags = {
     Name = "MyVPC"
   }
